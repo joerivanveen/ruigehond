435cd2090ee519de3ebbb18bde72ed40da5add8e
Joeri van Veen <nonstockphoto@gmail.com>
2019-05-28 14:05:03 +0200

    Initial commit (with code)
    Signed-off-by: Joeri van Veen <nonstockphoto@gmail.com>

---
+++ site/client/script.0.1.js
@@ -0,0 +1,5 @@
+function ruigehond(span) {
+    var apenstaart = '@';
+    var domain = window.location.host;
+    span.innerHTML = '<a href="mailto:hallo' + apenstaart + domain + '">E-mail: hallo' + apenstaart + domain + '</a>';
+}
---
+++ site/index2.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>ruige hond</title>
+    <link rel="canonical" href="https://ruigehond.nl"/>
+    <meta property="og:url" content="https://ruigehond.nl"/>
+    <meta property="og:site_name" content="ruige hond"/>
+    <meta property="og:title" content="ruige hond"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:description" content="Ruige hond = present online."/>
+    <meta property="og:image" content="http://ruigehond.nl/media/ruigehond.jpg"/>
+    <meta property="og:image:secure_url" content="https://ruigehond.nl/media/ruigehond.jpg"/>
+    <meta name="keywords" content="ruige hond present online"/>
+    <meta name="viewport" id="viewport" content="initial-scale=1.0"/>
+    <script type="text/javascript" src="/client/script.0.1.js" async></script>
+    <link rel="stylesheet" href="/client/style.0.1.css"/>
+</head>
+
+<body>
+
+<video autoplay="" loop="" poster="/media/blackfade.png" id="bgvid">
+    <source src="/media/joerivanveen1.webm" type="video/webm">
+    <source src="/media/joerivanveen1.mp4" type="video/mp4">
+</video>
+
+<img src="/media/logo-ruigehond.svg" alt="ruige hond"/><br/>
+<img src="/media/logo-ruigehond-leiband.svg" alt="leiband"/><br/>
+<img src="/media/logo-ruigehond-present-online.svg" alt="present online"/><br/>
+<div style="text-align: center;padding: 3em;">
+    Deze webpagina is nog eventjes in ontwikkeling.<br/>
+    ~<br/>
+    Wil jij ook een webpagina in ontwikkeling?<br/>
+    ~<br/>
+    <span onClick="ruigehond(this);" style="cursor:pointer;">Ja!</span>
+</div>
+</body>
